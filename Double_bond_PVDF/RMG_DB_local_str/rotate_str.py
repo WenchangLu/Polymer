fbe735a1819071b74d0b0187d27d2e84526e4bcc
WenchangLu <wlu2@ncsu.edu>
2024-01-11 15:56:43 -0900

    add lammps result for PEI

--- Double_bond_PVDF/RMG_DB_local_str/rotate_str.py
+++ Double_bond_PVDF/RMG_DB_local_str/rotate_str.py
@@ -113,7 +113,7 @@ for ro in range(0, 380, 15):
     y_max = max(atoms, key=lambda x:x[2])[2]
     z_max = max(atoms, key=lambda x:x[3])[3]
 
-    print(ro, x_min, x_max)
+    #print(ro, x_min, x_max)
     a_len = max(a_len, x_max-x_min)
     b_len = max(b_len, y_max-y_min)
     c_len = max(c_len, z_max-z_min)
@@ -135,5 +135,20 @@ for ro in range(0, 380, 15):
     filename = dir_name+"/input"
     with open(filename, "w") as f:
         f.write(input_lines)
+# 67 69
 
+    x0 = (atoms[67][1] + atoms[69][1]) * 0.5
+    y0 = (atoms[67][2] + atoms[69][2]) * 0.5
+    z0 = (atoms[67][3] + atoms[69][3]) * 0.5
+    x1 = atoms[1][1] - x0
+    y1 = atoms[1][2] - y0
+    z1 = atoms[1][3] - z0
+    x2 = atoms[134][1] - x0
+    y2 = atoms[134][2] - y0
+    z2 = atoms[134][3] - z0
+    tem = x1*x2 + y1 *y2 + z1 * z2
+    tem = tem/ math.sqrt(x1*x1+y1*y1+z1*z1)
+    tem = tem/ math.sqrt(x2*x2+y2*y2+z2*z2)
+    tem = 180 - math.acos(tem) /3.1415926 * 180
+    print("angle", ro, tem)
 print(a_len, b_len, c_len)

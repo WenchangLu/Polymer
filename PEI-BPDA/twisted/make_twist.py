98274bce35710ac8350514cbefa2e39604954ed7
WenchangLu <wlu2@ncsu.edu>
2025-05-06 13:58:48 -0400

    aa

--- PEI-BPDA/twisted/make_twist.py
+++ PEI-BPDA/twisted/make_twist.py
@@ -80,7 +80,8 @@ atoms_2_center = [[atom[0], atom[1]-x_center, atom[2]-y_center, atom[3]-z_center
 
 chain_dis = 8.0
 atoms_blend = []
-# chain along 100
+c = c_average
+# chain along 100 origin (0,0,0)
 for atom in atoms_1_center:
     x = atom[3]
     y = atom[2] 
@@ -89,108 +90,50 @@ for atom in atoms_1_center:
 
 # chain along 001 + origin from (c,0,0)
 for atom in atoms_1_center:
-    x = atom[1] + c_average
+    x = atom[1] + c
     y = atom[2] 
     z = atom[3] 
     atoms_blend.append([atom[0], x,y,z])
 
-# chain along 010 + origin from (c,0,c)
+# chain along -100+ origin from (c,0,c)
 for atom in atoms_1_center:
-    x = atom[1] + c_average
-    y = atom[3] 
-    z = atom[2] + c_average
+    x = -atom[3] + c
+    y = atom[2] 
+    z = atom[1] +c
     atoms_blend.append([atom[0], x,y,z])
 
-# chain along 001 + origin from (c,c,c)
-for atom in atoms_1_center:
-    x = atom[1] + c_average
-    y = atom[2] + c_average
-    z = atom[3] + c_average
-    atoms_blend.append([atom[0], x,y,z])
-
-# chain along -100 + origin from (c,c,2c)
-for atom in atoms_1_center:
-    x = -atom[3] + c_average
-    y = atom[2] + c_average
-    z = atom[1] + 2.0* c_average
-    atoms_blend.append([atom[0], x,y,z])
-
-# chain along 001 + origin from (0,c,2c)
+# chain along 001 + origin from (0,0,c)
 for atom in atoms_1_center:
     x = atom[1] 
-    y = atom[2] + c_average
-    z = atom[3] + 2.0* c_average
+    y = atom[2] 
+    z = atom[3] + c 
     atoms_blend.append([atom[0], x,y,z])
 
-# chain along 0-10 + origin from (0,0,2c)
+# chain along 100+ origin from (0,0,2c)
 for atom in atoms_1_center:
-    x = atom[1] 
-    y = -atom[3] +c_average
-    z = atom[2] + 3.0* c_average
+    x = atom[3] 
+    y = atom[2] 
+    z = atom[1] +2.0*c
+    atoms_blend.append([atom[0], x,y,z])
+
+# chain along 001 + origin from (c,0,2c)
+for atom in atoms_1_center:
+    x = atom[1] + c
+    y = atom[2] 
+    z = atom[3] + 2.0*c 
+    atoms_blend.append([atom[0], x,y,z])
+# chain along -100+ origin from (c,0,3c)
+for atom in atoms_1_center:
+    x = -atom[3] + c
+    y = atom[2] 
+    z = atom[1] +3.0*c
     atoms_blend.append([atom[0], x,y,z])
 
 # chain along 001 + origin from (0,0,3c)
 for atom in atoms_1_center:
     x = atom[1] 
     y = atom[2] 
-    z = atom[3] + 3.0* c_average
-    atoms_blend.append([atom[0], x,y,z])
-
-# second chains
-# chain along 010 + origin from (c/2,-c/2,c/2)
-for atom in atoms_1_center:
-    x = atom[1] + c_average *0.5
-    y = atom[3] - c_average *0.5
-    z = atom[2] + c_average *0.5
-    atoms_blend.append([atom[0], x,y,z])
-
-# chain along 001 + origin from (c/2, c/2,c/2)
-for atom in atoms_1_center:
-    x = atom[1] + c_average *0.5
-    y = atom[2] + c_average *0.5
-    z = atom[3] + c_average *0.5
-    atoms_blend.append([atom[0], x,y,z])
-
-# chain along 100 + origin from (c/2, c/2,3c/2)
-for atom in atoms_1_center:
-    x = atom[3] + c_average *0.5
-    y = atom[2] + c_average *0.5
-    z = atom[1] + c_average *1.5
-    atoms_blend.append([atom[0], x,y,z])
-
-# chain along 001 + origin from (3c/2, c/2,3c/2)
-for atom in atoms_1_center:
-    x = atom[1] + c_average *1.5
-    y = atom[2] + c_average *0.5
-    z = atom[3] + c_average *1.5
-    atoms_blend.append([atom[0], x,y,z])
-
-# chain along 0-10 + origin from (3c/2, c/2,5c/2)
-for atom in atoms_1_center:
-    x = atom[1] + c_average *1.5
-    y = -atom[3] + c_average *0.5
-    z = atom[2] + c_average *2.5
-    atoms_blend.append([atom[0], x,y,z])
-
-# chain along 001 + origin from (3c/2, -c/2,5c/2)
-for atom in atoms_1_center:
-    x = atom[1] + c_average *1.5
-    y = atom[2] - c_average *0.5
-    z = atom[3] + c_average *2.5
-    atoms_blend.append([atom[0], x,y,z])
-
-# chain along -100 + origin from (3c/2, -c/2,7c/2)
-for atom in atoms_1_center:
-    x = -atom[3] + c_average *1.5
-    y = atom[2] - c_average *0.5
-    z = atom[1] + c_average *3.5
-    atoms_blend.append([atom[0], x,y,z])
-
-# chain along 001 + origin from (c/2, -c/2,7c/2)
-for atom in atoms_1_center:
-    x = atom[1] + c_average *0.5
-    y = atom[2] - c_average *0.5
-    z = atom[3] + c_average *3.5
+    z = atom[3] + 3.0*c 
     atoms_blend.append([atom[0], x,y,z])
 
 
